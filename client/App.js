9e29f779efd60ce56817671e0e08764df801561c
amineBenFraj <mohamedamine.benfredj@polytechnicien.tn>
2024-08-05 16:02:51 +0100

    init

---
+++ client/App.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function App() {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        axios.get('/api')
+            .then(response => {
+                setData(response.data);
+            })
+            .catch(error => {
+                console.error("There was an error fetching the data!", error);
+            });
+    }, []);
+
+    return (
+        <div className="App">
+            <h1>Data from API</h1>
+            <ul>
+                {data.map(item => (
+                    <li key={item._id}>{item.name}</li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default App;
---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect('your_mongo_db_connection_string', { useNewUrlParser: true, useUnifiedTopology: true });
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully");
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
